79abe7b16c11ca4d18f77006ce095bde3aacf533
AndyLiu <andyliumathematics@163.com>
2021-03-12 15:28:31 +0800

    成功

---
+++ netty-demo-client/src/main/java/com/ll/netty/client/NettyClient.java
@@ -0,0 +1,23 @@
+package com.ll.netty.client;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+public class NettyClient {
+    public static void main(String[] args) throws InterruptedException {
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+        Bootstrap bootstrap = new Bootstrap();
+        bootstrap.group(workerGroup).channel(NioSocketChannel.class)
+        .handler(new ChannelInitializer<SocketChannel>(){
+            public void initChannel(SocketChannel ch){
+                ch.pipeline().addLast(new NettyClientHandler());
+            }
+        });
+
+        ChannelFuture future = bootstrap.connect("127.0.0.1",8756).sync();
+    }
+}
---
+++ netty-demo-server/src/main/java/com/ll/netty/server/NettyServer.java
@@ -0,0 +1,25 @@
+package com.ll.netty.server;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+public class NettyServer {
+    public static void main(String[] args) {
+        NioEventLoopGroup bootGroup = new NioEventLoopGroup();
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+        serverBootstrap.group(bootGroup, workerGroup).channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    protected void initChannel(NioSocketChannel ch) {
+                        ch.pipeline().addLast(new NettyServerHandler());
+                    }
+
+                });
+
+        serverBootstrap.bind(8756);
+
+    }
+}
