9760e9e7b7c33b5e75ba2e5529a980181567918e
AndyLiu <andyliumathematics@163.com>
2021-03-16 16:23:51 +0800

    add

--- netty-demo-client/src/main/java/com/ll/netty/client/NettyClient.java
+++ netty-demo-client/src/main/java/com/ll/netty/client/NettyClient.java
@@ -1,5 +1,8 @@
 package com.ll.netty.client;
 
+import java.time.LocalDate;
+import java.util.Date;
+
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
@@ -19,5 +22,11 @@ public class NettyClient {
         });
 
         ChannelFuture future = bootstrap.connect("127.0.0.1",8756).sync();
+        future.addListener(future1 -> {
+            if (future1.isSuccess())
+                System.out.println("成功"+new Date().toString());
+            else
+                System.err.println("链接失败");
+        });
     }
 }
